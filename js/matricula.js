9deeabd058b588eabdf044e3e2932f4c62939a0d
Rota-Classe-Alta <114256137+Rota-Classe-Alta@users.noreply.github.com>
2022-11-19 15:40:45 -0300

    Add files via upload

---
+++ js/matricula.js
@@ -0,0 +1,234 @@
+var capturando = "";
+        
+        function capturar() {
+            capturando = document.querySelector('#valor').value;
+            var daniloLima = '000001-22';
+            var joseVinicios = '000003-22';
+            var joaoNascimento = '000004-22';
+            var henriqueThompsom = '000012-22';
+            var andreyMota = '000006-22';
+            var cauaBarbosa = '000028-22';
+            var daviDantas = '000016-22';
+            var harley = '000009-22';
+            var joaoPedro = '000025-22';
+            var oliveiraLopes = '000029-22';
+            var bruceRocket = '000022-22';
+            var bergsonRamos = '000013-22';
+            var lucasNeri = '000018-22';
+            var guilhermePgnatari = '000021-22';
+            var andreyBeck = '000024-22';
+
+            switch (capturando) {
+                case daniloLima: matriculaLima();
+                    
+                    break;
+
+                case joseVinicios: matriculaJose();
+                    
+                    break;
+
+                case joaoNascimento: matriculaNascimento();
+
+                    break;
+
+                case henriqueThompsom: matriculaThompsom();
+
+                    break;
+
+                case andreyMota: matriculaAndreyMota();
+
+                    break;
+
+                case cauaBarbosa: matriculaCauaBarbosa();
+
+                    break;
+
+                case daviDantas: matriculaDantas();
+
+                    break;
+
+                case harley: matriculaHarley();
+
+                    break;
+
+                case joaoPedro: matriculaJoaoPedro();
+
+                    break;
+
+                case oliveiraLopes: matriculaOliveiraLopes();
+
+                    break;
+                
+                case bruceRocket: matriculaBruceRocket();
+
+                    break;
+                
+                case bergsonRamos: matriculaBergsonRamos();
+
+                    break;
+                
+                case lucasNeri: matriuclaLucasNeri();
+
+                    break;
+
+                case guilhermePgnatari: matriculaGuilhermePg()
+
+                    break
+
+                case andreyBeck: matriculaAndreyBack()
+
+                default:
+
+                    break;
+            };
+
+            function matriculaLima() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/LIMA.png";
+            };
+
+            function matriculaJose() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/VINI.png";
+            };
+
+            function matriculaNascimento() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/NASCIMENTO.png";
+            };
+
+            function matriculaThompsom() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/THOMSPON.png";
+            };
+
+            function matriculaAndreyMota() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/andreyMOTA.png";
+            };
+
+            function matriculaCauaBarbosa() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/CAUABARBOSA.png";
+            };
+
+            function matriculaDantas() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/DANTAS.png";  
+            };
+
+            function matriculaHarley() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/HARLEY.png";  
+            };
+
+            function matriculaJoaoPedro() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/JOAOPEDRO.png"; 
+            };
+            
+            function matriculaOliveiraLopes() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/oliveiraLopes.png";
+            };
+
+            function matriculaBruceRocket() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/bruceRocket.png";
+            };
+
+            function matriculaBergsonRamos() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/bergsonRamos.png"; 
+            };
+
+            function matriuclaLucasNeri() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/lucasneri.png"; 
+            };
+
+            function matriculaGuilhermePg() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/guilhermePg.png"; 
+            };
+
+            function matriculaAndreyBack() {
+                document.getElementById('bemvindo').style.display = 'block';
+
+                document.getElementById('valor').style.display = 'none';
+                document.getElementById('pesquisar').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+
+                document.getElementById('img2').src="../images/matriculas/ANDREY_BECKHAM.png";
+            };
+        };
